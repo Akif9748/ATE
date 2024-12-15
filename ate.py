cd7901a48d59e23f85468de91c50b0fe996e948c
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2024-12-15 04:11:04 +0300

    Matematik

---
+++ ate.py
@@ -0,0 +1,14 @@
+"""
+F(x)=(3x+10)/2
+
+[6 bileşkeli F(x)]/2+5=x
+
+"""
+
+def f(x): # gün sonu kalan su
+    return x-((x/2)+5) + x
+
+
+for x in range(0, 20):
+    print(x,f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(x))))))))))))))))))))))))))))))/2+5)
+
